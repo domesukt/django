190d5e25c2ed27857422ea4f473d5292bd917b77
domesukt <domesu.koyo@gmail.com>
2020-02-22 18:05:52 +0900

    '三章'

--- pages/urls.py
+++ pages/urls.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import path
-from .views import homePageView
+from .views import homePageView, HomePageView
 
 urlpatterns = [
-    path('', homePageView, name='home')  # URLが呼び出すViewを記述
-    
+    #URLが呼び出すViewを記述する
+    path('kawa', homePageView, name='kawa'), #関数の場合
+    path('', HomePageView.as_view(), name='home')   #クラスの場合
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 def homePageView(request):
     return HttpResponse('Hello Kawa')
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
